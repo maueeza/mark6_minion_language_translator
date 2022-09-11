dcd353fdb0b687895c30c8f60d7d246ac0ef5f97
Maueeza Bhatkar <90929270+maueeza@users.noreply.github.com>
2022-09-11 19:36:21 +0530

    feat : added banana translation api
    Added final api for minion language translator

--- app.js
+++ app.js
@@ -3,17 +3,37 @@
 var ClickBtn = document.querySelector("#btn-click");
 ClickBtn.addEventListener("click", clickHandler)
 //input
-var inpText = document.querySelector("#inp-textarea");
+var inpTextArea = document.querySelector("#inp-textarea");
 
 //output
 var outputDiv = document.querySelector("#output-text");
 
 //processing
+
+function errorHandler(error)
+{
+    console.log("Error occured ",error);
+    alert("Something wrong with server! Please try again after sometime! ");
+}
+
+function constructURL(txt)
+    {
+        //var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+        var serverURL = "https://api.funtranslations.com/translate/minion.json";
+        return serverURL+"?"+"text="+txt;
+    }
+
 function clickHandler()
 {
-    console.log("Clicked!");
+    /*console.log("Clicked!");
     console.log(inpText.value);
-    outputDiv.innerText=inpText.value;
+    outputDiv.innerText=inpText.value;*/
 
+    var InpTxt = inpTextArea.value;
+        fetch(constructURL(InpTxt))
+        .then(response=>response.json())
+        .then(json=> outputDiv.innerHTML = json.contents.translated)
+        .catch(errorHandler)
+    
     
 }
