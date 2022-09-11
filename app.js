783ab9bd2da33307eb17a1aff3b79f37e5cc5e81
Maueeza Bhatkar <90929270+maueeza@users.noreply.github.com>
2022-09-11 16:41:11 +0530

    feat : added textarea
    Added textarea with onclick functionality for mark6 : ex04

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
 
 var ClickBtn = document.querySelector("#btn-click");
-ClickBtn.addEventListener("click", function clickHandler()
+ClickBtn.addEventListener("click", clickHandler)
+
+var inpText = document.querySelector("#inp-textarea");
+console.log(inpText);
+
+function clickHandler()
 {
     console.log("Clicked!");
-})
+    console.log(inpText.value);
+}
